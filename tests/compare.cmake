409d2090f7c250898f4344c13dde3431f6f821f2
dtaliun <dtaliun@umich.edu>
2017-11-24 22:03:34 -0500

    issue #2. automatic tests

---
+++ tests/compare.cmake
@@ -0,0 +1,17 @@
+
+if(EXISTS METAANALYSIS1.TBL)
+    file(REMOVE METAANALYSIS1.TBL)
+endif()
+if(EXISTS METAANALYSIS1.TBL.info)
+    file(METAANALYSIS1.TBL.info)
+endif()
+
+execute_process(COMMAND ${METAL} metal.txt RESULT_VARIABLE metal_exit_code)
+if(metal_exit_code)
+    message(FATAL_ERROR "METAL failed.")
+endif()
+
+execute_process(COMMAND diff -q METAANALYSIS1.TBL ${METAL_TBL} RESULT_VARIABLE diff_exit_code)
+if(diff_exit_code)
+    message(FATAL_ERROR "METAL didn't replicate results.")
+endif()
